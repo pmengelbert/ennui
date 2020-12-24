86940d8ee6442c4a5e7cd035cd056248c49de860
Peter Engelbert <pmengelbert@gmail.com>
2020-12-24 14:04:45 -0600

    Integrate server. Add makefile
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
+target/release/ennui: clean src/mapdata.rs
+	cargo build --release --bin ennui
 
+.PHONY clean:
+	rm target/release/convert
+	rm target/release/ennui
+	rm src/mapdata.rs
+	echo "pub const MAP: [u8; 0] = [];" > src/mapdata.rs
 
-convert:
+target/release/convert:
 	cargo build --release --bin convert
 
-build-map: convert
+src/mapdata.rs: target/release/convert
 	target/release/convert sample.yaml src/mapdata.rs
 
-all: build-map
-	cargo build --release --bin ennui
---
+++ src/bin/server.rs
@@ -0,0 +1,100 @@
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc::{channel, Sender, Receiver};
+use std::sync::{Mutex, Arc};
+use std::thread::spawn;
+use std::io::{Read, Write};
+
+use ennui::game::Game;
+use ennui::player::Player;
+
+macro_rules! arc_mutex(
+    ($wrapped:expr) => {
+        Arc::new(Mutex::new($wrapped))
+    };
+);
+
+trait ReadLine {
+    fn read_line(&mut self) -> std::io::Result<String>;
+}
+
+impl<T> ReadLine for T
+    where
+        T: Read,
+{
+    fn read_line(&mut self) -> std::io::Result<String> {
+        let mut buf = [0u8; 256];
+        let n = self.read(&mut buf)?;
+        Ok(
+            String::from_utf8((&buf[..n-1]).to_owned()).unwrap()
+        )
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8089")?;
+
+    let g = Game::new();
+    let shared_game = arc_mutex!(g);
+    let mut join_handles = vec![];
+
+    for stream in listener.incoming() {
+        let game_clone = shared_game.clone();
+
+        let mut stream = stream?;
+        write!(stream, "enter your name: ")?;
+        let name = stream.read_line()?;
+
+        let p = Player::new_with_stream(&name, stream.try_clone().unwrap());
+        let uuid = p.uuid();
+        {
+            let mut game = match game_clone.lock() {
+                Ok(g) => g,
+                Err(err) => {
+                    println!("error: {}", err);
+                    continue;
+                }
+            };
+            game.broadcast(format!("{} has joined the game.", name))?;
+            game.add_player(p);
+        }
+
+        let stream_clone = stream.try_clone()?;
+
+        join_handles.push(
+            spawn(move || handle_client(stream_clone, uuid, game_clone))
+        );
+    }
+
+    for handle in join_handles {
+        handle.join().unwrap();
+    }
+
+    Ok(())
+}
+
+fn handle_client<T: ReadLine + Write>(mut stream: T, p: u128, g: Arc<Mutex<Game>>) -> std::io::Result<()> {
+    stream.write(b" > ")?;
+    loop {
+        let s = stream.read_line()?;
+
+        {
+            let mut g = match g.lock() {
+                Ok(g) => g,
+                Err(err) => {
+                    println!("error: {}", err);
+                    break;
+                }
+            };
+
+            let resp = g.interpret(p, &s);
+            if let Some(response) = resp {
+                g.send_to_player(p, format!("\n{}\n\n > ", response))?;
+            } else {
+                g.remove_player(p);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
