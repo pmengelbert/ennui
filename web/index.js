093e3837113558d90e6eee9f3518b1fd80107e7c
Peter Engelbert <pmengelbert@gmail.com>
2021-01-16 01:21:32 -0600

    Multiplayer via the browser
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- web/index.js
+++ web/index.js
@@ -1,17 +1,45 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const net = require('net');
+const Convert = require('ansi-to-html');
 
-module.exports = {
-   run: async function (s) {
-      let x = import('./pkg').then(r =>
-          r.interpret(s)
-      );
-      let y = await x;
-      return y;
-   },
-   color: function (s) {
-      var Convert = require('ansi-to-html');
-      var convert = new Convert();
+const convert = new Convert();
 
-      return convert.toHtml(s);
-   }
-};
 
+function color(s) {
+    return convert.toHtml(s);
+}
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    let id = Math.floor(Math.random() * 101);
+    socket.emit('id', id);
+
+    console.log('a user connected');
+    var client = new net.Socket();
+    client.connect(8089, '127.0.0.1', function() {
+        console.log('connected to 8089');
+    });
+
+    client.on('data', function(data) {
+        console.log(data.toString());
+        socket.emit('chat message', color(data.toString()));
+    });
+
+    socket.on('chat message', (msg) => {
+        client.write(msg + '\n');
+    });
+});
+
+io.on('disconnect', (socket) => {
+    console.log('a user disconnected');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
