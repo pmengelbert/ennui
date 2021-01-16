fbe11550ed0cf7521c970991fad2d9549399b25b
Peter Engelbert <pmengelbert@gmail.com>
2021-01-16 02:09:58 -0600

    runs in multiple environments
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- web/index.js
+++ web/index.js
@@ -1,44 +1,59 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 const net = require('net');
 const Convert = require('ansi-to-html');
+const fs = require('fs');
+const https = require('https');
 
-const convert = new Convert();
+function run(srv, port) {
+	const io = require('socket.io')(srv);
 
-function color(s) {
-    return convert.toHtml(s);
+	const convert = new Convert();
+
+	function color(s) {
+	    return convert.toHtml(s);
+	}
+
+	app.get('/', (req, res) => {
+	    res.sendFile(__dirname + '/index.html');
+	});
+
+	io.on('connection', (socket) => {
+	    let id = Math.floor(Math.random() * 101);
+	    socket.emit('id', id);
+
+	    console.log('a user connected');
+
+	    var client = new net.Socket();
+	    client.connect(8089, '127.0.0.1', function() {
+		console.log('connected to 8089');
+	    });
+
+	    client.on('data', function(data) {
+		console.log(data.toString());
+		socket.emit('chat message', color(data.toString()));
+	    });
+
+	    socket.on('chat message', (msg) => {
+		client.write(msg + '\n');
+	    });
+		
+	    socket.on('disconnect', (socket) => {
+		console.log('a user disconnected');
+	    });
+	});
+
+	srv.listen(port, () => {
+	    console.log('listening on *:' + port);
+	});
 }
 
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-    let id = Math.floor(Math.random() * 101);
-    socket.emit('id', id);
-
-    console.log('a user connected');
-    var client = new net.Socket();
-    client.connect(8089, '127.0.0.1', function() {
-        console.log('connected to 8089');
-    });
-
-    client.on('data', function(data) {
-        console.log(data.toString());
-        socket.emit('chat message', color(data.toString()));
-    });
-
-    socket.on('chat message', (msg) => {
-        client.write(msg + '\n');
-    });
-});
-
-io.on('disconnect', (socket) => {
-    console.log('a user disconnected');
-});
-
-http.listen(3000, () => {
-    console.log('listening on *:3000');
-});
+if (process.env.ENVIRONMENT === "production") {
+	const privateKey = fs.readFileSync('/etc/letsencrypt/live/poobuttz.lol/privkey.pem', 'utf8');
+	const certificate = fs.readFileSync('/etc/letsencrypt/live/poobuttz.lol/fullchain.pem', 'utf8');
+	const credentials = {key: privateKey, cert: certificate};
+	const httpsServer = https.createServer(credentials, app);
+	run(httpsServer, 443);
+} else {
+	run(http, 3000);
+}
